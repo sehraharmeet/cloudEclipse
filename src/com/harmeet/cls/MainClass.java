c3e5c72c3546660503b0184f31405718f5c92e2c
sehraharmeet <150445048+sehraharmeet@users.noreply.github.com>
2024-08-25 10:26:55 +0530

    harmeet first commit

---
+++ src/com/harmeet/cls/MainClass.java
@@ -0,0 +1,12 @@
+package com.harmeet.cls;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.println("Log("+ i + ")=" + Math.log(i)   );
+		}
+		
+	}
+
+}
